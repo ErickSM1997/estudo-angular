825c3ac8edd7afb64377940ad806b9a57407a085
Erick Souza <158108903+ErickSM1997@users.noreply.github.com>
2025-08-24 16:41:42 -0300

    Criando rotas dos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ServicoComponent } from './servico/servico.component';
+import { CadastroFuncionarioComponent } from './cadastro-funcionario/cadastro-funcionario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"cadastro",component:CadastroComponent},
+  {path:"servico",component:ServicoComponent},
+  {path:"cadastro-funcionario",component:CadastroFuncionarioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { ServicoComponent } from './servico/servico.component';
+import { CadastroFuncionarioComponent } from './cadastro-funcionario/cadastro-funcionario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CadastroComponent
+    CadastroComponent,
+    ServicoComponent,
+    CadastroFuncionarioComponent
   ],
   imports: [
     BrowserModule,
